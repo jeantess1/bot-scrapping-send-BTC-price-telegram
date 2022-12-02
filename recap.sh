bcb193458e3c0b897ecea896755fbf119c1bdb91
jeantess1 <105298691+jeantess1@users.noreply.github.com>
2022-12-02 10:30:32 +0100

    Update recap.sh
    issue w/ moyenne

--- recap.sh
+++ recap.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 declare -a tableau_indi
 count=0
->teeet.txt
 somme=0
-while IFS=" " read -ra ligne
+moyenne=0
+fichier=/home/ec2-user/projet_scraping/Prix.txt
+while read line
 do
+        count=$(echo "$count+1" | bc)
+        somme=$(echo "$somme + $line" | bc)
+        moyenne=$(echo "scale=2;$somme / $count" | bc)
 
-        count=$(echo "$count + 1" | bc)
-        somme=$(echo "$somme + $ligne" | bc)
-        echo $ligne
-done<<< $(cat Prix.txt)
-moyenne=$(echo "$somme / $count" | bc)
+done<"$fichier"
 valeurentree=$(cat Prix.txt | head -1)
 valeursortie=$(cat Prix.txt | tail -1)
-echo $valeurentree
-echo $valeursortie
-echo $moyenne
+max=$(cat Prix.txt| sort -n | tail -1)
+min=$(cat Prix.txt| sort -n | head -1)
+dat=$(date)
 CHAT_ID=-1001876276898
-curl -s --data chat_id=$CHAT_ID --data-urlencode "text=Valeur d entrée= $valeurentree
-Valeur sortie= $valeursortie
-Moyenne=$moyenne" "https://api.telegram.org/bot5722099198:AAHR87VgvswTd19OhyuWxOqJ9x7x0S7nZ4E/sendMessage?parse_mode"
->Prix.txt
+curl -s --data chat_id=$CHAT_ID --data-urlencode "text=$dat
+
+Valeur d'entrée=\$$valeurentree
+Valeur sortie= \$$valeursortie
+Valeur Min= \$$min
+Valeur Max= \$$max
+Moyenne=\$$moyenne" "https://api.telegram.org/bot5722099198:AAHR87VgvswTd19OhyuWxOqJ9x7x0S7nZ4E/sendMessage?parse_mode"
+
+>$fichier
